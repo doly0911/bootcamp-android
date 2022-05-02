92c71705e788f8cef91a2c9310ebd4e4c282c2f3
Doly Jimenez <doly.jimenez11@gmail.com>
2022-05-02 06:56:31 -0500

    add input for console and change price format of worked hours

--- salaryapp/src/main/kotlin/Main.kt
+++ salaryapp/src/main/kotlin/Main.kt
@@ -1,18 +1,27 @@
-import employee.Employee
 import employee.EmployeeSelector
 import employee.IEmployeeSelector
 import salary.ISalaryCalculatorSelector
 import salary.SalaryCalculatorSelector
+import java.text.NumberFormat
+import java.util.Scanner
 
 fun main(args: Array<String>) {
-    val role:Int= 2
-    val workedHours: Int= 180
+
+    // Creates an instance which takes input from standard input (keyboard)
+    val reader = Scanner(System.`in`)
+    print("Enter a role: ")
+    // nextInt() reads the next integer from the keyboard
+    var role:Int = reader.nextInt()
+
+    print("Enter worked hours: ")
+    var workedHours:Int = reader.nextInt()
 
     val employeeSelector: IEmployeeSelector = EmployeeSelector()
     val employee = employeeSelector.selectEmployee(role)
 
     val salarySelector: ISalaryCalculatorSelector = SalaryCalculatorSelector()
     val salaryCalculator = salarySelector.selectSalaryCalculator(employee, workedHours)
+    val numHours = NumberFormat.getInstance().format(salaryCalculator.calculateSalary(employee, workedHours));
 
-    println("The salary ${employee.javaClass.name} is ${salaryCalculator.calculateSalary(employee, workedHours)} ")
+    println("The salary ${employee.javaClass.name} is $numHours")
 }
