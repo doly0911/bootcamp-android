21833e31e152f3c0bd5093af43018ab1831764da
Doly Jimenez <doly.jimenez11@gmail.com>
2022-05-09 22:43:45 -0500

    Change bound service to play audio

--- lifeCycle/app/src/main/java/com/bootcamp/myapplication/MainActivity.kt
+++ lifeCycle/app/src/main/java/com/bootcamp/myapplication/MainActivity.kt
@@ -4,13 +4,12 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mService: MyService
@@ -20,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     private val connection = object : ServiceConnection {
 
         override fun onServiceConnected(className: ComponentName, service: IBinder) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            // We've bound to MyService, cast the IBinder and get MyService instance
             val binder = service as MyService.MyServiceBinder
             mService = binder.getService()
             mBound = true
@@ -34,27 +33,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn = findViewById(R.id.btn_crearServicio)
+        btn = findViewById(R.id.btn_play_audio)
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
 
         btn.setOnClickListener {
             if (mBound) {
-                // Call a method from the LocalService.
-                // However, if this call were something that might hang, then this request should
-                // occur in a separate thread to avoid slowing down the activity performance.
-                val num: Int = mService.randomNumber
-                Toast.makeText(this, "number: $num", Toast.LENGTH_SHORT).show()
+                // Call a method from the MyService.
+               mService.playAudio()
             }
         }
     }
-
-
 
     override fun onStart() {
         super.onStart()
         Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
 
-        // Bind to LocalService
+        // Bind to MyService
         Intent(this, MyService::class.java).also { intent ->
             bindService(intent, connection, Context.BIND_AUTO_CREATE)
         }
--- lifeCycle/app/src/main/java/com/bootcamp/myapplication/MyService.kt
+++ lifeCycle/app/src/main/java/com/bootcamp/myapplication/MyService.kt
@@ -6,14 +6,17 @@ import android.os.Binder
 import android.os.IBinder
 import android.widget.Toast
 import java.util.*
+import android.media.MediaPlayer
+
+
+
 
 class MyService: Service() {
 
     // Binder given to clients
     private val binder = MyServiceBinder()
 
-    // Random number generator
-    private val mGenerator = Random()
+    private lateinit var myPlayer:MediaPlayer
 
     inner class MyServiceBinder: Binder(){
         // Return this instance of MyService
@@ -26,24 +29,31 @@ class MyService: Service() {
     }
 
     /** method for clients  */
-    val randomNumber: Int
-        get() = mGenerator.nextInt(100)
+    fun playAudio(){
+        myPlayer.start()
+    }
 
     override fun onCreate() {
         super.onCreate()
         Toast.makeText(this, "onCreate Service", Toast.LENGTH_SHORT).show()
+        myPlayer = MediaPlayer.create(this, R.raw.rington_super_mario_bros)
+        myPlayer.setOnCompletionListener { it.release() }
+        myPlayer.isLooping = false // Set looping
     }
 
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+        Toast.makeText(this, "onRebind Service", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onUnbind(intent: Intent?): Boolean {
         Toast.makeText(this, "onUnbind Service", Toast.LENGTH_SHORT).show()
         return super.onUnbind(intent)
-
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Toast.makeText(this, "onDestroy Service", Toast.LENGTH_SHORT).show()
-
+        myPlayer.stop();
     }
 }
