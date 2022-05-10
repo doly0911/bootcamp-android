9c0a3f72f405cee1116e9b6a5231441a29e72e97
Doly Jimenez <doly.jimenez11@gmail.com>
2022-05-10 09:21:20 -0500

    implement increase function and layout

--- lifeCycle/app/src/main/java/com/bootcamp/myapplication/GoodByeActivity.kt
+++ lifeCycle/app/src/main/java/com/bootcamp/myapplication/GoodByeActivity.kt
@@ -2,10 +2,57 @@ package com.bootcamp.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class GoodByeActivity : AppCompatActivity() {
+    private lateinit var txtNum: TextView
+    private var num : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_good_bye)
+        txtNum = findViewById(R.id.increasedNumber)
+        num = 0
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onResume() {
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+        super.onResume()
+
+    }
+
+    override fun onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+        super.onPause()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    fun increase(view: View) {
+        num++
+        txtNum.text = num.toString()
     }
 }
